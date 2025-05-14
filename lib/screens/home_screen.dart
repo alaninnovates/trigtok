5fbbbde01ad53957b1e399bc6627d6b1bed7f131
alaninnovates <alanlichen1@gmail.com>
2025-05-14 15:30:33 -0700

    Implement left off on indicator

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,7 +14,9 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final _future = Supabase.instance.client
       .from('profiles_classes')
-      .select('classes(id, name)');
+      .select(
+        'classes(id, name), profiles(study_timelines(units(class_id, name), created_at))',
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +35,10 @@ class _HomeScreenState extends State<HomeScreen> {
               child: FutureBuilder(
                 future: _future,
                 builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    print(snapshot.error);
+                    return Center(child: const Text('Error loading classes'));
+                  }
                   if (!snapshot.hasData) {
                     return const Center(child: CircularProgressIndicator());
                   }
@@ -58,10 +64,19 @@ class _HomeScreenState extends State<HomeScreen> {
                     itemCount: classes.length,
                     itemBuilder: (context, index) {
                       final classItem = classes[index];
+                      final leftOffUnit =
+                          classItem['profiles']['study_timelines'][0]['units'];
+                      var leftOffUnitName = '';
+                      if (leftOffUnit['class_id'] ==
+                          classItem['classes']['id']) {
+                        leftOffUnitName = leftOffUnit['name'];
+                      } else {
+                        leftOffUnitName = 'Unknown';
+                      }
                       return Card(
                         child: ListTile(
                           title: Text(classItem['classes']['name']),
-                          subtitle: Text('Left off on: unknown'),
+                          subtitle: Text('Left off on: $leftOffUnitName'),
                           onTap: () {
                             GoRouter.of(
                               context,
