a2fd98fa6f5ae4b09bbfdeafab94bd9cf5bd63fb
alaninnovates <alanlichen1@gmail.com>
2025-06-30 23:32:39 -0400

    Start working on bookmarks screen

--- lib/screens/profile-flow/bookmarks_screen.dart
+++ lib/screens/profile-flow/bookmarks_screen.dart
@@ -1,10 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class BookmarksScreen extends StatelessWidget {
+class BookmarksScreen extends StatefulWidget {
   const BookmarksScreen({super.key});
 
   @override
+  State<BookmarksScreen> createState() => _BookmarksScreenState();
+}
+
+class _BookmarksScreenState extends State<BookmarksScreen> {
+  late Future<List<Map<String, dynamic>>> _future;
+  int _page = 1;
+  final int _limit = 5;
+  bool _hasMore = true;
+  List<Map<String, dynamic>> _bookmarks = [];
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    if (_isLoading || !_hasMore) return;
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final from = (_page - 1) * _limit;
+      final response = await Supabase.instance.client
+          .from('study_timelines')
+          .select(
+            // todo optimize this query when i figure out what data is needed
+            '*, explanations(*), free_response_questions(*), multiple_choice_questions(*), topics(*)',
+          )
+          .eq('bookmark', true)
+          .range(from, from + _limit - 1)
+          .order('created_at', ascending: false);
+
+      setState(() {
+        if (response.isEmpty || response.length < _limit) {
+          _hasMore = false;
+        }
+        _bookmarks.addAll(response);
+        _page++;
+        _isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: const Center(child: Text('Bookmarks Screen')));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Bookmarks')),
+      body: NotificationListener<ScrollNotification>(
+        onNotification: (ScrollNotification scrollInfo) {
+          if (!scrollInfo.metrics.atEdge) return false;
+          if (scrollInfo.metrics.pixels == 0) return false; // Top edge
+          _loadData();
+          return true;
+        },
+        child: ListView.builder(
+          itemCount: _bookmarks.length + (_hasMore ? 1 : 0),
+          itemBuilder: (context, index) {
+            if (index >= _bookmarks.length) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            final bookmark = _bookmarks[index];
+            return ListTile(title: Text(bookmark.toString()));
+          },
+        ),
+      ),
+    );
   }
 }
